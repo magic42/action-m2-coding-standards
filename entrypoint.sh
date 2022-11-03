fa07bca5e977bbe8f007eb24c82657f249bd9109
sam-dugmore <sam.dugmore@magic42.co.uk>
2022-11-03 11:37:51 +0000

    Update action to add path variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,21 +8,25 @@ echo "::add-matcher::${HOME}/problem-matcher.json"
 cd $GITHUB_WORKSPACE
 
 test -z "${PHPCS_STANDARD}" && PHPCS_STANDARD=$INPUT_PHPCS_STANDARD
+test -z "${PHPCS_PATH}" && PHPCS_PATH=$INPUT_PHPCS_PATH
 test -z "${PHPCS_SEVERITY}" && PHPCS_SEVERITY=$INPUT_PHPCS_SEVERITY
 test -z "${PHPCS_REPORT}" && PHPCS_REPORT=$INPUT_PHPCS_REPORT
 test -z "${PHPCS_EXTENSIONS}" && PHPCS_EXTENSIONS=$INPUT_PHPCS_EXTENSIONS
 
 test -z "${PHPCS_STANDARD}" && PHPCS_STANDARD=Magento2
+test -z "${PHPCS_PATH}" && PHPCS_PATH="app/code/Magic42"
 test -z "${PHPCS_SEVERITY}" && PHPCS_SEVERITY=8
 test -z "${PHPCS_REPORT}" && PHPCS_REPORT=checkstyle
 test -z "${PHPCS_EXTENSIONS}" && PHPCS_EXTENSIONS=php
 
 echo "PHPCS report: ${PHPCS_REPORT}"
 echo "PHPCS standard: ${PHPCS_STANDARD}"
+echo "PHPCS path: ${PHPCS_PATH}"
 echo "PHPCS severity: ${PHPCS_SEVERITY}"
 echo "PHPCS severity: ${PHPCS_EXTENSIONS}"
 
 sh -c "/root/.composer/vendor/bin/phpcs \
+  ${PHPCS_PATH}
   --report=${PHPCS_REPORT} \
   --extensions=${PHPCS_EXTENSIONS} \
   --severity=${PHPCS_SEVERITY} \
